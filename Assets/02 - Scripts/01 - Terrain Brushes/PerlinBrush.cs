622bf556580339f209ca9b7ad41a7a6369d2e5e7
lauraminkova <laura.minkova@yahoo.ca>
2022-10-15 19:21:14 +0200

    working perlin brush

--- Assets/02 - Scripts/01 - Terrain Brushes/PerlinBrush.cs
+++ Assets/02 - Scripts/01 - Terrain Brushes/PerlinBrush.cs
@@ -4,22 +4,27 @@ using UnityEngine;
 
 public class PerlinBrush : TerrainBrush {
 
-    //TO COMPLETE
-    //https://docs.unity3d.com/ScriptReference/Mathf.PerlinNoise.html
-
-    public float amplitude = 1;
-    //add all params of the perlin brush
+    public float h = 0.5f;
+    public float s = 2.0f;
+    public float o = 0.0f;
+    public int maxN = 50;
 
     public override void draw(int x, int z) {
+
         for (int zi = -radius; zi <= radius; zi++) {
             for (int xi = -radius; xi <= radius; xi++) {
+                
+                float rand_max = Random.Range(0, maxN);
+                float perlin = 0;
+                for(int i = 0; i < rand_max; i++){
+                    perlin += Mathf.PerlinNoise(s*(xi+o), s*(zi+o));
+                }
 
-                float cur = terrain.get(x+xi, z+zi);
-                float h = amplitude * Mathf.PerlinNoise(xi, zi);// wrong -> might want to try with smaller stuff
-                // h = h* amplitude;
-                terrain.set(x + xi, z + zi, h);
+                float height = h * perlin;
+
+                terrain.set(x + xi, z + zi, height);
+
             }
         }
     }
 }
-
